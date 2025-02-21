7eba6a3177a5aea596b140b944fee517764e07ca
dagarces <dgarcesganau@gmail.com>
2025-02-21 10:19:56 +0100

    Modificacion de la clase principal.

--- PruebaGitHubSTS/src/main/Main.java
+++ PruebaGitHubSTS/src/main/Main.java
@@ -5,15 +5,18 @@ import entities.ObjectB;
 import entities.ObjectC;
 
 public class Main {
-
-	public static void main(String[] args) {
-
-		ObjectA objetoA = new ObjectA(1, "Pepito", "Grillo");
-		ObjectB objetoB = new ObjectB(1, "Alfredo", 25);
-		ObjectC objetoC = new ObjectC(1, "Ana", "Valencia");
-
-		System.out.println(objetoA.toString());
-		System.out.println(objetoB.toString());
-		System.out.println(objetoC.toString());
+	 public static void main(String[] args) {
+	 
+	 ObjectA objetoA = new ObjectA(1,"Pepito", "Grillo");
+	 ObjectB objetoB = new ObjectB(1,"Alfredo", 25);
+	 ObjectC objetoC = new ObjectC(1,"Ana", "Valencia");
+	 
+	 System.out.println(objetoA.toString());
+	 System.out.println(objetoB.toString());
+	 System.out.println(objetoC.toString());
+	 
+	 ObjectC objetoC2 = new ObjectC(2,"Antonia", "Almeria");
+	 System.out.println(objetoC2.toString());
+	 }
 	}
-}
+
